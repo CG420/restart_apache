6959ad491e2b34ba27a3cc3f74813dc98a332b9e
Chris Grooms <cgrooms21@aol.com>
2019-03-26 14:24:12 -0500

    Improved messaging

--- re2.sh
+++ re2.sh
@@ -7,3 +7,20 @@ then
     echo "$0 requires two parameters {virtual-host} {restart|reload}"
     exit 1
 fi
+
+#only allow reload or restart
+if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
+then
+    cd /etc/apache2/sites-available
+    # Disable a vhost
+    sudo a2dissite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+
+    #Enable 
+    sudo a2ensite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+else
+    echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
+    exit 1
+fi
+
